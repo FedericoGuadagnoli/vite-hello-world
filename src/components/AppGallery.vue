28a388c4b2dbab495c40b68af08ff515667950b8
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-19 14:32:05 +0100

    Create AppGallery component

---
+++ src/components/AppGallery.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'AppGallery',
+    data() {
+        return {
+            image: 'elden-ring-ranni.jpg'
+        }
+    },
+
+    methods: {
+        buildImagePath(image) {
+            const url = new URL(`../assets/img/${image}`, url);
+            return url.href;
+        }
+    }
+}
+</script>
+
+
+
+<template>
+    <img :src="buildImagePath(image)" alt="">
+</template>
+
+
+<style>
+
+</style>
